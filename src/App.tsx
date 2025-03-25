cf31e140b2694953751f2f5b728b773a2c540d10
Eric Kitaif <eric.kitaif@gmail.com>
2025-03-25 23:37:13 +0100

    Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import { Map } from "./components/Map";
 import { ConvertCoordinate } from "./components/ConvertCoordinate";
 import { Card } from "./components/Card";
 import { PageTemplate } from "./components/PageTemplate";
+import { Navbar } from "./components/Navbar";
 
 const App: Component = () => {
   return (
-    <PageTemplate headerText="Target Imagery Generator" class="container">
-      <Card headerText="Convert Coordinates" class="mb-4">
-        <ConvertCoordinate></ConvertCoordinate>
-      </Card>
-      <Map></Map>
-    </PageTemplate>
+    <>
+      <Navbar />
+
+      <PageTemplate headerText="Planning Tools" class="container">
+        <Card headerText="Convert Coordinates" class="mb-4">
+          <ConvertCoordinate></ConvertCoordinate>
+        </Card>
+        <Map></Map>
+      </PageTemplate>
+    </>
   );
 };
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,152 @@
+import { A } from "@solidjs/router";
+import { Component, createSignal, JSX } from "solid-js";
+
+type Props = Record<string, never>;
+
+const Item: Component<{ href: string; sidebar?: boolean; children?: JSX.Element }> = ({
+  href,
+  sidebar = false,
+  children,
+}) => {
+  const [active, setActive] = createSignal(false);
+  // const router = useRouter();
+
+  // const [mounted, setMounted] = createSignal(true);
+
+  // useEffect(() => setMounted(true), []);
+
+  // useEffect(() => {
+  //   if (!router) return;
+  //   setActive(router.pathname === href);
+  // }, [href, router]);
+
+  const className = () => {
+    if (active() && sidebar)
+      return `block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 active:bg-gray-500 
+      focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out`;
+    if (!active() && sidebar)
+      return `mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 active:bg-gray-500 hover:text-white 
+      hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out`;
+    if (active() && !sidebar)
+      return `ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-white bg-gray-900 active:bg-gray-500 
+      focus:outline-none focus:text-white focus:bg-gray-700  transition duration-150 ease-in-out`;
+    if (!active() && !sidebar)
+      return `ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 active:bg-gray-500 hover:text-white 
+    hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out`;
+  };
+
+  return (
+    <A href={href} class={className()}>
+      {children}
+    </A>
+  );
+};
+
+const Navbar: Component<Props> = () => {
+  // const { user, logout } = useAuth();
+  const [menuOpen, setMenuOpen] = createSignal(false);
+  // const [userMenuOpen, setUserMenuOpen] = createSignal(false);
+  // const {
+  //   scenarios: { activeScenario },
+  // } = useContext(DataContext);
+
+  // const profileMenu = useRef<HTMLDivElement>(null);
+
+  // createEffect(() => {
+  //   const handleClick = (e: any) => {
+  //     // if (profileMenu.current?.contains(e.target)) {
+  //     //   return;
+  //     // }
+  //     // setUserMenuOpen(false);
+  //   };
+
+  //   document.addEventListener("mousedown", handleClick);
+  //   return () => {
+  //     document.removeEventListener("mousedown", handleClick);
+  //   };
+  // });
+
+  return (
+    <div>
+      <nav class="bg-gray-800">
+        <div class="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8 not-printed">
+          <div class="relative flex items-center justify-between h-16">
+            <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              {/* <!-- Mobile menu button--> */}
+              <button
+                class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
+                aria-label="Main menu"
+                aria-expanded="false"
+                onClick={() => {
+                  setMenuOpen(!menuOpen());
+                }}
+              >
+                {/* <!-- Icon when menu is closed. -->*/}
+                <svg
+                  class={`${menuOpen() ? "hidden" : "block"} h-6 w-6`}
+                  stroke="currentColor"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+                {/* <!-- Icon when menu is open. -->*/}
+                <svg
+                  class={`${menuOpen() ? "block" : "hidden"} h-6 w-6`}
+                  stroke="currentColor"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
+            <div class="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
+              <div class="flex-shrink-0">
+                <img class="block w-auto h-8 lg:hidden" src="/bombing-run.svg" alt="Workflow logo" />
+                <img class="hidden w-auto h-8 lg:block" src="/bombing-run.svg" alt="Workflow logo" />
+              </div>
+              <div class="hidden sm:block sm:ml-6">
+                <div class="flex items-center">
+                  <Item href="/">Planning Tools</Item>
+                  <Item href="https://dieselplanning.com">Old Dieselplanning</Item>
+                  {/* <Item href="/data">Scenario</Item>
+                  <Item href="/tools">Tools</Item>
+                  <Item href="/help">Help</Item> */}
+                </div>
+              </div>
+            </div>
+            <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              {/* <!-- Profile dropdown --> */}
+              <Item href="/login">Account</Item>
+            </div>
+          </div>
+        </div>
+
+        {menuOpen() && (
+          <div>
+            <div class="px-2 pt-2 pb-3">
+              <Item href="/" sidebar>
+                Planning Tools
+              </Item>
+              <Item href="https://dieselplanning.com" sidebar>
+                Old Dieselplanning
+              </Item>
+              {/* <Item href="/data" sidebar>
+                Scenario
+              </Item>
+              <Item href="/tools" sidebar>
+                Tools
+              </Item>
+              <Item href="/help" sidebar>
+                Help
+              </Item> */}
+            </div>
+          </div>
+        )}
+      </nav>
+    </div>
+  );
+};
+
+export { Navbar };
