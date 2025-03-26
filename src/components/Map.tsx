220b5eb5b5bcb9a72c5970de14f332d28804399e
Eric Kitaif <eric.kitaif@gmail.com>
2025-03-26 13:20:39 +0100

    Update Map.tsx
    Remove GEOJSON

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -21,20 +21,6 @@ const Map: Component = () => {
   const color = "black";
   const moaSymbol = new SimpleFillSymbol({ style: "none", outline: { color } });
   const renderer = new SimpleRenderer({ symbol: moaSymbol });
-  const layers = [
-    new GeoJSONLayer({ url: "/geojson/Korea_LLZs.geojson", geometryType: "polygon", renderer }),
-    new GeoJSONLayer({ url: "/geojson/2NM_Buffer.geojson", geometryType: "polygon", renderer }),
-    new GeoJSONLayer({ url: "/geojson/Current_BMGR_May_2020.geojson", geometryType: "polygon", renderer }),
-    new GeoJSONLayer({ url: "/geojson/Korea_NFL.geojson", geometryType: "polygon", renderer }),
-    new GeoJSONLayer({ url: "/geojson/Low_MOAs.geojson", geometryType: "polygon", renderer }),
-    new GeoJSONLayer({ url: "/geojson/MOAs.geojson", geometryType: "polygon", renderer }),
-    new GeoJSONLayer({ url: "/geojson/New_BMGR.geojson", geometryType: "polygon", renderer }),
-    new GeoJSONLayer({ url: "/geojson/P_518_Border.geojson", geometryType: "polygon", renderer }),
-    new GeoJSONLayer({ url: "/geojson/Restricted_Areas.geojson", renderer }),
-    new GeoJSONLayer({ url: "/geojson/D35.geojson", renderer }),
-  ];
-
-  map.addMany(layers);
 
   onMount(() => {
     view = new MapView({
