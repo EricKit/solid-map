57a8e5d2921b1ab9b98d24ffbe6b8bfbef8c1a10
Eric Kitaif <eric.kitaif@gmail.com>
2025-03-27 13:21:32 +0100

    Trying different perforamnce

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -42,6 +42,7 @@ const Map: Component = () => {
       style: mapStyle as StyleSpecification,
       center: settings.startCenter.lngLatArray,
       zoom: 7, // starting zoom
+      canvasContextAttributes: { powerPreference: "low-power", contextType: "webgl" },
     });
 
     map.addControl(
